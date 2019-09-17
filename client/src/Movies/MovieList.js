0cc5354e6a681a2c41696f1b176188d186dd1d97
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-09-17 13:08:09 -0700

    PART 2 -- first pt

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import axios from 'axios';
 
 const MovieList = props => {
@@ -30,6 +32,8 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to ={`/movie-list/${movie.id}`}> 
+
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +50,8 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    
+    </Link> 
   );
 }
 
